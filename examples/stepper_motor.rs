aba77bb3e9fa7be13f55754856a03a8d0868d2b5
9names <60134748+9names@users.noreply.github.com>
2021-10-17 22:24:10 +1100

    Improve stepper sequence + documentation

--- examples/stepper_motor.rs
+++ examples/stepper_motor.rs
@@ -5,6 +5,10 @@
 //! This will rotate a two-phase, 4 wire stepper motor
 //! attached to Motor1 and Motor 2 screw terminals
 //!
+//! Note that if you halt the processor, you may leave a GPIO high and burn out your motor.
+//! Hold the boot button and press reset if any light is still remaining to avoid this.
+//! Treat this as a friendly reminder to use the watchdog for any unattended installation to avoid fires.
+//!
 //! See the `Cargo.toml` file for Copyright and licence details.
 
 #![no_std]
@@ -97,31 +101,56 @@ fn main() -> ! {
     motor2_b.set_low().unwrap();
 
     // How long between changing phases (do not go less than 4)
-    const DELAY: u32 = 5;
+    const DELAY: u32 = 10;
     const STEPS_PER_CYCLE: usize = 256;
+
+    for _ in 0..STEPS_PER_CYCLE {
+        // step clockwise
+        motor1_a.set_high().unwrap();
+        delay.delay_ms(DELAY);
+        motor1_a.set_low().unwrap();
+
+        motor2_a.set_high().unwrap();
+        delay.delay_ms(DELAY);
+        motor2_a.set_low().unwrap();
+
+        motor1_b.set_high().unwrap();
+        delay.delay_ms(DELAY);
+        motor1_b.set_low().unwrap();
+
+        motor2_b.set_high().unwrap();
+        delay.delay_ms(DELAY);
+        motor2_b.set_low().unwrap();
+    }
+
+    for _ in 0..STEPS_PER_CYCLE {
+        // step counter-clockwise
+        motor2_a.set_high().unwrap();
+        delay.delay_ms(DELAY);
+        motor2_a.set_low().unwrap();
+
+        motor1_a.set_high().unwrap();
+        delay.delay_ms(DELAY);
+        motor1_a.set_low().unwrap();
+
+        motor2_b.set_high().unwrap();
+        delay.delay_ms(DELAY);
+        motor2_b.set_low().unwrap();
+
+        motor1_b.set_high().unwrap();
+        delay.delay_ms(DELAY);
+        motor1_b.set_low().unwrap();
+    }
+
+    // Motor demo done, blink an LED now.
+    // These little motors are quite strong, but are not designed to be
+    // left running for long periods of time. A quick demo is best.
+    let mut led = pins.grove_1_a.into_push_pull_output();
     loop {
-        for _ in 0..STEPS_PER_CYCLE {
-            // step clockwise
-            motor1_a.set_high().unwrap();
-            delay.delay_ms(DELAY);
-            motor1_a.set_low().unwrap();
-            delay.delay_ms(DELAY);
-            motor1_b.set_high().unwrap();
-            delay.delay_ms(DELAY);
-            motor1_b.set_low().unwrap();
-            delay.delay_ms(DELAY);
-        }
-        for _ in 0..STEPS_PER_CYCLE {
-            // step counter clockwise
-            motor2_a.set_high().unwrap();
-            delay.delay_ms(DELAY);
-            motor2_a.set_low().unwrap();
-            delay.delay_ms(DELAY);
-            motor2_b.set_high().unwrap();
-            delay.delay_ms(DELAY);
-            motor2_b.set_low().unwrap();
-            delay.delay_ms(DELAY);
-        }
+        led.set_high().unwrap();
+        delay.delay_ms(50);
+        led.set_low().unwrap();
+        delay.delay_ms(50);
     }
 }
 
